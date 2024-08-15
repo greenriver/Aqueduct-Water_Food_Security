23a547354121f6b5b6c4f6dc93f906ffb06d414d
Carlos Del Real <carlos.delreal.5@wriconsultant.org>
2024-08-15 10:08:04 -0500

    fix: water pop ups

--- src/components/map/utils.js
+++ src/components/map/utils.js
@@ -2,14 +2,21 @@ import { WATER_INDICATORS_METADATA } from './constants';
 
 export const parseMetadataLayer = (layer = {}) => {
   if (layer.dataset === '979e6a3e-3905-401f-ab81-cd83c5c50c6c' || layer.dataset === '3cca5144-63d2-4ddb-af53-094603b702f3') {
-    const { description, sources } = WATER_INDICATORS_METADATA[layer.slug];
+    let { slug } = layer;
+
+    if (slug.slice(slug.length - 2) === '_1') {
+      slug = slug.slice(0, slug.length - 2);
+    }
+
+    const { description, sources } = WATER_INDICATORS_METADATA[slug];
+
     return ({
       ...layer,
       metadata: {
         ...layer.metadata,
         ...description && { description },
         info: {
-          ...layer.metadata.info,
+          ...layer?.metadata?.info && { info: layer.metadata.info },
           ...sources && { sources }
         }
       }
